faa24e95784b083cc6dfa8e45681278700dcd830
Amaan <82905413+awake141327@users.noreply.github.com>
2023-06-16 00:09:01 +0530

    Update Coins.jsx

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -86,7 +86,7 @@ const CoinCard = ({id, name, img, price, symbol, currencySymbol = "₹"}) => {
                 }}
             > 
                 <Image src={img} w={"10"} h={"10"} objectFit={"contain"} alt={"Exchange"} />
-                <Heading size={"md"} noOfLines={1}>{symbol}</Heading>
+                <Heading textTransform={"uppercase"} size={"md"} noOfLines={1}>{symbol}</Heading>
                 <Text noOfLines={1}>{name}</Text>
                 <Text noOfLines={1}>{price ? `${currencySymbol}${price}`: 'NA'}</Text>
 
